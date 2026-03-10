81502e17b9b8f02193ae267481636767c808f146
Albert <90371111+albertjp00@users.noreply.github.com>
2026-03-10 09:28:05 +0530

    updated env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ dotenv.config();
 
 
 app.use(cors({
-  origin : "http://localhost:5173",
+  origin : process.env.frontend_url,
   methods : ["GET" , "POST" , "PUT" , "PATCH" , "DELETE"],
   credentials : true
 }));
